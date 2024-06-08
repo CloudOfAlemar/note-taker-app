7687bab5b2eafecb7cc2b231340c1d4a8eabcf3f
Alejandro Martinez <cloudof.alemar@gmail.com>
2024-06-07 17:03:18 -0700

    1). Created app.js and included the express.static middleware to serve static files as well as connected the Get Started button with /notes.html file.

---
+++ app.js
@@ -0,0 +1,16 @@
+
+const express = require( "express" );
+const path = require( "path" );
+
+const app = express();
+const port = 3000;
+
+app.use( express.static( path.join( `${ __dirname }`, "public" ) ) );
+
+app.get( "/notes", ( req, res ) => {
+  res.sendFile( path.join( `${ __dirname }`, "public", "notes.html" ) );
+} );
+
+app.listen( port, () => {
+  console.log( `Listening on port ${ port }...` );
+} );
