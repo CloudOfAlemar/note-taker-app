15af61b8c5ad6856bcc3695b2b9c0893587b08d9
Alejandro Martinez <cloudof.alemar@gmail.com>
2024-06-11 16:01:34 -0700

    Corrected the Port Number.

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const express = require( "express" );
 const { v4 : uuidv4 } = require( "uuid" );
 
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 3001;
 
 app.use( express.json() );
 app.use( express.static( path.join( `${ __dirname }`, "public" ) ) );
@@ -53,6 +53,6 @@ app.get( "*", ( req, res ) => {
   res.sendFile( path.join( `${ __dirname }`, "public", "index.html" ) );
 } );
 
-app.listen( port, () => {
+app.listen( PORT, () => {
   console.log( `Listening on port ${ port }...` );
 } );
