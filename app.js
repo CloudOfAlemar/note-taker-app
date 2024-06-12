fa3002ddb5fb6a431812b93104d0092562e02fec
Alejandro Martinez <cloudof.alemar@gmail.com>
2024-06-11 17:27:22 -0700

    Added Comments in app.js.

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-
+/*
+  Requiring Modules
+*/
 const fs = require( "fs/promises" );
 const path = require( "path" );
 const express = require( "express" );
@@ -7,13 +9,28 @@ const { v4 : uuidv4 } = require( "uuid" );
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+/*
+  Middleware
+*/
 app.use( express.json() );
 app.use( express.static( path.join( `${ __dirname }`, "public" ) ) );
 
+/*
+  Get Endpoint to /notes
+    1). Serving the notes.html file from the public folder
+*/
 app.get( "/notes", ( req, res ) => {
   res.sendFile( path.join( `${ __dirname }`, "public", "notes.html" ) );
 } );
 
+/*
+  Get Endpoint to /api/notes
+    1). Read db.json file
+    2). Respond with data as JSON
+    3). Check for error types
+      - file not found
+      - server error
+*/
 app.get( "/api/notes", ( req, res ) =>  {
   fs.readFile( `${ __dirname }/db/db.json`, "utf-8" )
   .then( data => res.status( 200 ).json( JSON.parse( data ) ) )
@@ -26,6 +43,15 @@ app.get( "/api/notes", ( req, res ) =>  {
   } );
 } );
 
+/*
+  Post Endpoint to /api/notes
+    1). Read db.json file
+    2). Parse the data being read into JSON
+    3). Give the request body a unique ID using uuid package
+    4). Write the new Note into db.json
+    5). Then send a success message
+    6). Check for error
+*/
 app.post( "/api/notes", ( req, res ) => {
   fs.readFile( `${ __dirname }/db/db.json`, "utf-8" )
   .then( data => {
@@ -38,6 +64,15 @@ app.post( "/api/notes", ( req, res ) => {
   .catch( error => error );
 } );
 
+/*
+  Delete Endpoint to /api/notes/:id
+    1). Read db.json file
+    2). Parse data into an Object
+    3). Find the notes that don't match the req.params.id
+      - we'll write those to the db.json file
+    4). Write the updated notes to db.json file
+    5). Send a JSON response of Note deleted
+*/
 app.delete( "/api/notes/:id", ( req, res ) => {
   fs.readFile( `${ __dirname }/db/db.json`, "utf-8" )
   .then( data => {
@@ -49,10 +84,14 @@ app.delete( "/api/notes/:id", ( req, res ) => {
   .catch( error => error );
 } );
 
+/*
+  Wildcard / Catch All Route
+    1). Send index.html file when a req hits this route
+*/
 app.get( "*", ( req, res ) => {
   res.sendFile( path.join( `${ __dirname }`, "public", "index.html" ) );
 } );
 
 app.listen( PORT, () => {
-  console.log( `Listening on port ${ port }...` );
+  console.log( `Listening on port ${ PORT }...` );
 } );
